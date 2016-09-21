0ba222b29d4a8c8758f3f8dcac40820d009f58fd
Dushara Jayasinghe <silent.correspondent@gmail.com>
2016-09-22 08:05:35 +1000

    Create script to check git commit messages
    This script checks against rules 1-4 and 6 as specified in
    
    	http://chris.beams.io/posts/git-commit/
    
    Copy the commit-msg.sh script to .git/hooks/commit-msg in your
    projects to enables these checks.
    
    Sample text files included for testing purposes

---
+++ commit-msg.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+filename="$1"
+lineno=0
+
+error() {
+    echo "$1"
+    exit 1
+}
+
+while read -r line
+do
+    let lineno+=1
+    length=${#line}
+
+    # Subject line tests
+    if [[ $lineno -eq 1 ]]; then
+        [[ $length -gt 50 ]] && error "Limit the subject line to 50 characters"
+        [[ ! "$line" =~ ^[A-Z].*$ ]] && error "Capitalise the subject line"
+        [[ "$line" == *. ]] && error "Do not end the subject line with a period"
+    fi
+
+    # Rules related to the commit message body
+    [[ $lineno -eq 2 ]] && [[ -n $line ]] && error "Separate subject from body with a blank line"
+    [[ $lineno -gt 1 ]] && [[ $length -gt 72 ]] && error "Wrap the body at 72 characters"
+done < "$filename"
+exit 0
